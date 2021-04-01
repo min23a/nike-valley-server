db86d55296a1c89a27613710062892218444dbbb
min23a <minhazuloabedin0611@gmail.com>
2021-04-01 22:50:17 +0600

    port and nodemon  update

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 app.use(cors());
 app.use(express.json());
+const port = process.env.PORT || 5000;
 
-app.listen('3001', console.log(3001));
+app.listen(port);
 
 app.get('/',(req,res) => {
     res.send("Hello")
